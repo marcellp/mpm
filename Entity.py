00c3a6bc420da277632839869e7041fbcb85ac5d
Myles Lamb <mylesalamb@gmail.com>
2018-10-12 22:21:46 +0100

    Altered entity class, new backpack and stats

--- Entity.py
+++ Entity.py
@@ -1,13 +1,17 @@
 class Entity:
 
-    def __init__(self,actions={},stats={}):
+    def __init__(self,actions={},stats={},inventory=[]):
 
+        self.inventory = []
         self.actions = actions
         if stats == {}:
-            self.stats  ={"level":0,"Strength":0,"Perception":0,"Endurance":0,
+            self.stats  ={"Level":0,"Strength":0,"Perception":0,"Endurance":0,
                           "Charisma":0,"Intelligence":0,"Agility":0,"Luck":0}
         else:
             self.stats = stats
+
+        if actions == {}:
+            self.actions = {}
 
 
     def print_stats(self):

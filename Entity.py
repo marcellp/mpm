a852048c02ff8cbf400fab388a1b3efab64a4861
Myles Lamb <mylesalamb@gmail.com>
2018-10-12 22:16:38 +0100

    Altered entity class

--- Entity.py
+++ Entity.py
@@ -1,13 +1,18 @@
 class Entity:
 
-    def __init__(self,actions={},state={}):
+    def __init__(self,actions={},stats={}):
 
         self.actions = actions
-        self.state = state
+        if stats == {}:
+            self.stats  ={"level":0,"Strength":0,"Perception":0,"Endurance":0,
+                          "Charisma":0,"Intelligence":0,"Agility":0,"Luck":0}
+        else:
+            self.stats = stats
+
 
     def print_stats(self):
         """print the state of the entity"""
-        print(self.state)
+        print(self.stats)
 
     def save(self):
         """save the the current state of the object to file as json"""

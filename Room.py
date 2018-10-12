3d021488668ca285c85d3d6749bd6846cee25e33
Marcell Pek <2333134p@student.gla.ac.uk>
2018-10-12 22:50:06 +0000

    Change room description and add extra properties

--- Room.py
+++ Room.py
@@ -1,11 +1,24 @@
+from IOStream import io
+
 class Room(object):
-	def __init__(self, name):
+	def __init__(self, name, interior = True):
 		self.name = name
+		self.interior = interior
+
 		self.paths = {"up": None, "down": None, "left": None, "right": None}
 		self.description = "You see an empty room"
 		self.items = []
 		self.entities = []
+		self.locked = False
 
 	def describe(self):
-		print(self.name.upper())
-		print(self.description)
+		io.out(self.name.upper())
+		io.out(self.description)
+		io.out("")
+
+		directions = [direction for (direction, room) in self.paths.items() if room]
+
+		if not directions:
+			io.out('It appears there is no way out of this room.')
+
+		io.out("You can go {}.".format(', '.join(directions)))

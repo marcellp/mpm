4f6fd8025cbfa3f625a3447a618b21b3966ce372
Myles Lamb <mylesalamb@gmail.com>
2018-10-13 11:29:48 +0100

    changes to item

--- Item.py
+++ Item.py
@@ -2,16 +2,22 @@ from enum import Enum
 
 class Item(object):
 
-    def __init__(self,name,desc,weight,effect):
+    def __init__(self,name,desc,weight,effect,inventory=None):
         self.name = name
         self.desc = desc
         self.weight = weight
         self.effect = effect
+        self.item_code = item_code
+        self.inventory = inventory
 
     ### add types that effect what item does
 
 
-class type(Enum):
-    food = 1
+class Item_type(Enum):
+    food_hp = 1
+    food_ap = 2
+    wearable= \
+        3
+
 
 

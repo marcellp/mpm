8a30a75d7418aaf9eda942175f78b2f8a553ba7a
Marcell Pek <2333134p@student.gla.ac.uk>
2018-10-13 11:20:38 +0000

    Modify room logic

--- Room.py
+++ Room.py
@@ -1,12 +1,20 @@
 from IOStream import io
 
 class Room(object):
-	def __init__(self, name, interior = True):
-		self.name = name
-		self.interior = interior
 
-		self.paths = {"up": None, "down": None, "left": None, "right": None}
-		self.description = "You see an empty room"
+	ALLOWED_PATHS = ('up', 'down', 'left', 'right', 'north', 'south', 'east', 'west')
+
+	def __init__(self, name, description = None):
+		self.name = name
+		self.interior = True
+		self.paths = {direction : None for direction in Room.ALLOWED_PATHS}
+
+		if not description:
+			self.description = "You see an empty room"
+		else:
+			self.description = description
+
+		self.path_text = ""
 		self.items = []
 		self.entities = []
 		self.locked = False
@@ -18,7 +26,17 @@ class Room(object):
 
 		directions = [direction for (direction, room) in self.paths.items() if room]
 
-		if not directions:
+		if self.path_text:
+			io.out(self.path_text)
+		elif not self.paths:
 			io.out('It appears there is no way out of this room.')
+		else:
+			io.out("You can go {}.".format(', '.join(directions)))
 
-		io.out("You can go {}.".format(', '.join(directions)))
+	def get_room_at(self, direction):
+		direction = direction.lower()
+
+		if direction not in Room.ALLOWED_PATHS:
+			return False
+
+		return self.paths[direction]

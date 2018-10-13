a6dc461ea3eb8effcaf5df8398ab0867bd6ff7b7
Marcell Pek <2333134p@student.gla.ac.uk>
2018-10-13 11:24:39 +0000

    Add parser

---
+++ Parser.py
@@ -0,0 +1,47 @@
+from IOStream import io
+from Room import Room
+
+class Parser(object):
+
+	def __init__(self, game):
+		self.game = game
+
+	def parse_move(self, words):
+		if words[0] != 'go' and words[0] != 'move':
+			return None
+
+		if len(words) != 2:
+			io.out('USAGE: [move/go] [direction]')
+			return False
+
+		direction = words[1].lower()
+
+		if direction not in Room.ALLOWED_PATHS:
+			io.out('That is not a valid direction to move to.')
+			return False
+
+		next_room = self.game.p.at.get_room_at(direction)
+
+		if not next_room:
+			io.out('There is nothing in that direction.')
+			return False
+
+		if next_room.locked:
+			io.out('The room is locked and you cannot go in.')
+			return False
+
+		self.game.p.at = next_room
+		next_room.describe()
+		return True
+
+	def parse_inventory(self, words):
+		if len(words) != 1 or (words[0] != 'inventory' and words[0] != 'inv'):
+			return None
+
+		self.game.p.show_inventory()
+		return True
+
+	def parse(self, str):
+		words = str.strip().split()
+
+		not self.parse_move(words) and not self.parse_inventory(words)
